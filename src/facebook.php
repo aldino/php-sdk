85ce529affcb4da9923bb9478797318e93cccc50
aldino <aldino_bravo@yahoo.co.id>
2011-06-21 22:19:12 -0700

    Edited src/facebook.php via GitHub

--- src/facebook.php
+++ src/facebook.php
@@ -1,21 +1,93 @@
- <?php 
+<?php
+/**
+ * Copyright 2011 Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
 
-     $app_id = "224170954274036";
+require_once "base_facebook.php";
 
-     $canvas_page = "http://apps.facebook.com/make_rupiah/";
+/**
+ * Extends the BaseFacebook class with the intent of using
+ * PHP sessions to store user ids and access tokens.
+ */
+class Facebook extends BaseFacebook
+{
+  /**
+   * Identical to the parent constructor, except that
+   * we start a PHP session to store the user ID and
+   * access token if during the course of execution
+   * we discover them.
+   *
+   * @param Array $config the application configuration.
+   * @see BaseFacebook::__construct in facebook.php
+   */
+  public function __construct($config) {
+    if (!session_id()) {
+      session_start();
+    }
+    parent::__construct($config);
+  }
 
-     $auth_url = "https://www.facebook.com/dialog/oauth?client_id=224170954274036
-     &redirect_uri=http://apps.facebook.com/make_rupiah/&scope=email,read_stream
+  protected static $kSupportedKeys =
+    array('state', 'code', 'access_token', 'user_id');
 
-     $signed_request = $_REQUEST["signed_request"];
+  /**
+   * Provides the implementations of the inherited abstract
+   * methods.  The implementation uses PHP sessions to maintain
+   * a store for authorization codes, user ids, CSRF states, and
+   * access tokens.
+   */
+  protected function setPersistentData($key, $value) {
+    if (!in_array($key, self::$kSupportedKeys)) {
+      self::errorLog('Unsupported key passed to setPersistentData.');
+      return;
+    }
 
-     list($encoded_sig, $payload) = explode('.', $signed_request, 2); 
+    $session_var_name = $this->constructSessionVariableName($key);
+    $_SESSION[$session_var_name] = $value;
+  }
 
-     $data = json_decode(base64_decode(strtr($payload, '-_', '+/')), true);
+  protected function getPersistentData($key, $default = false) {
+    if (!in_array($key, self::$kSupportedKeys)) {
+      self::errorLog('Unsupported key passed to getPersistentData.');
+      return $default;
+    }
 
-     if (empty($data["user_id"])) {
-            echo("<script> top.location.href='" . $auth_url . "'</script>");
-     } else {
-            echo ("Welcome User: " . $data["user_id"]);
-     } 
- ?>
+    $session_var_name = $this->constructSessionVariableName($key);
+    return isset($_SESSION[$session_var_name]) ?
+      $_SESSION[$session_var_name] : $default;
+  }
+
+  protected function clearPersistentData($key) {
+    if (!in_array($key, self::$kSupportedKeys)) {
+      self::errorLog('Unsupported key passed to clearPersistentData.');
+      return;
+    }
+
+    $session_var_name = $this->constructSessionVariableName($key);
+    unset($_SESSION[$session_var_name]);
+  }
+
+  protected function clearAllPersistentData() {
+    foreach (self::$kSupportedKeys as $key) {
+      $this->clearPersistentData($key);
+    }
+  }
+
+  protected function constructSessionVariableName($key) {
+    return implode('_', array('fb',
+                              $this->getAppId(),
+                              $key));
+  }
+}
